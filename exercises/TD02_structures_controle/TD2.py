64254ff06cefc672aaed6a6a313ba9b25ed96743
uvsq22103840 <114145374+uvsq22103840@users.noreply.github.com>
2022-09-29 11:15:20 +0200

    Create TD2.py

---
+++ exercises/TD02_structures_controle/TD2.py
@@ -0,0 +1,18 @@
+Exercice 1:
+num1=15
+num2=5
+if num1%num2 == 0 and num2%num1 == 0:
+  print(Factor)
+Exercice 2: 
+state= "Georgia"
+if state=="New Jersey":
+  print("School isn't cancelled.")
+elif state=="North Carolina":
+  print("School is postponed.")
+elif state=="Georgia":
+  print("School is cancelled!")
+else:
+  print("School's status is unknown")
+Exercice 3 : 
+annee = int(input("saisir une annee"))
+modulo = annee % 12
