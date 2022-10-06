fed5c7d05b228d541702b8f301089b06e4087fce
uvsq22103840 <114145374+uvsq22103840@users.noreply.github.com>
2022-10-06 12:41:48 +0200

    Create TD1.py

---
+++ TD1.py
@@ -0,0 +1,56 @@
+
+"Exercice 1" 
+1+1
+a=1+1
+print(a)
+a=a+1
+print(a)
+a=a+1
+print(a)
+"Exercice2" 
+1+1
+a=1+1
+print(a)
+a=a+1
+print(a)
+a=a+1
+print(a)
+a=5
+b=2
+print(a,b)
+a,b=b,a
+print(a,b)
+c=5
+d=2
+a=d
+b=c
+print(a,b)
+print("a vaut", a, "et b vaut", b)
+a="Hello World"
+print(a)
+
+"Exercice 3"
+print(type(5))
+print(type(5.0))
+print(type("5"))
+print(type(-5))
+print(type(5 / 2))
+print(type(5 // 2))
+print(type(5 == 2))
+print(type(5=2))
+print(type(5, 2))
+print(type[5, 2])
+print(type{5, 2})
+x=1
+print(type(x))
+v=22
+v//11
+print("=",v//11,"x 11+",v%11)
+a= 3 * (10 / 3)
+print("=",a,)
+b=3 * (10 // 3)
+print("=",b)
+c=(3 * 10) // 3
+print("=",c)
+var1=1
+print((var1%2)==0)
